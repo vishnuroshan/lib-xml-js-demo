160ed4231c554ea5dd673ba33f06f2107772e003
Vishnu Roshan <32105473+vishnuroshan@users.noreply.github.com>
2022-07-26 13:24:37 +0530

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,25 @@
+#! /usr/bin/env node
+const yargs = require('yargs');
+const utils = require('./utils.js');
 
+const usage = '\nUsage: libxmldemo <xml_file_path> <option>';
+const options = yargs
+  .usage(usage)
+  .option('c', { alias: 'create', describe: 'create xml file with data', type: 'boolean', demandOption: false })
+  .option('arc', {
+    alias: 'add-random-child',
+    describe: 'add random-child to xml file',
+    type: 'boolean',
+    demandOption: false,
+  })
+
+  .help(true).argv;
+
+
+if (yargs.argv._[0] == null) {
+  utils.showHelp();
+  return;
+}
+var xml = utils.printXMLfromfile(yargs.argv);
+
+console.log(xml)
